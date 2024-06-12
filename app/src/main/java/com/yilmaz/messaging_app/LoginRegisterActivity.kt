4c33302bc5cf8c9a8d5951a4a96bb35d7720d5e4
Ahmed Utku ÖZÜDOĞRU <ahmedutkuozudogru@gmail.com>
2024-06-12 22:22:00 +0300

    More Functionality added to Login&Register

--- app/src/main/java/com/yilmaz/messaging_app/LoginRegisterActivity.kt
+++ app/src/main/java/com/yilmaz/messaging_app/LoginRegisterActivity.kt
@@ -19,6 +19,9 @@ class LoginRegisterActivity : AppCompatActivity() {
         Log.d("LoginRegisterActivity", "binding: $binding")
         if (savedInstanceState == null) {
             replaceFragment(Login())
+            binding.registerButton.visibility = Button.VISIBLE
+            binding.loginButton.visibility = Button.GONE
+
         }
 
         binding.registerButton.setOnClickListener(){
@@ -26,16 +29,12 @@ class LoginRegisterActivity : AppCompatActivity() {
             binding.textView.setText("Already have an account?")
             binding.registerButton.visibility = Button.GONE
             binding.loginButton.visibility = Button.VISIBLE
-            /*binding.registerButton.isEnabled = false
-            binding.loginButton.isEnabled = true*/
         }
         binding.loginButton.setOnClickListener(){
             replaceFragment(Login())
             binding.textView.setText("Don't have an account?")
             binding.registerButton.visibility = Button.VISIBLE
             binding.loginButton.visibility = Button.GONE
-            /*binding.registerButton.isEnabled = true
-            binding.loginButton.isEnabled = false*/
 
         }
     }
